bf72a37ecea2c7188a901337ecc4cfa3b25161b5
DevMazed04 <mdmazedislam2684@gmail.com>
2022-08-23 08:30:50 +0600

    selected-v.js file added and add event listener to the select buttons

---
+++ js/selected-v.js
@@ -0,0 +1,33 @@
+/* -------------player-1 selected------------- */
+document.getElementById('btn-select-1').addEventListener('click', function () {
+    const player1Element = document.getElementById('player-1-name');
+    const player1Name = player1Element.innerText;
+
+    const playerSelectedList = document.getElementById('player-selected-list');
+    const li = document.createElement('li');
+    li.innerText = player1Name;
+    playerSelectedList.appendChild(li);
+})
+
+/* -------------player-2 selected------------- */
+document.getElementById('btn-select-2').addEventListener('click', function () {
+    const player2Element = document.getElementById('player-2-name');
+    const player2Name = player2Element.innerText;
+
+    const playerSelectedList = document.getElementById('player-selected-list');
+    const li = document.createElement('li');
+    li.innerText = player2Name;
+    playerSelectedList.appendChild(li);
+})
+
+/* -------------player-3 selected------------- */
+document.getElementById('btn-select-3').addEventListener('click', function () {
+    const player3Element = document.getElementById('player-3-name');
+    const player3Name = player3Element.innerText;
+
+    const playerSelectedList = document.getElementById('player-selected-list');
+    const li = document.createElement('li');
+    li.innerText = player3Name;
+    playerSelectedList.appendChild(li);
+})
+
